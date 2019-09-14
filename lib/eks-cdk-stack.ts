2bdeface468c294a032737e1996dcb4e41e626b8
HeRoMo <hero@asterisk-works.jp>
2019-09-14 15:15:58 +0900

    add VPC

--- lib/eks-cdk-stack.ts
+++ lib/eks-cdk-stack.ts
@@ -1,9 +1,26 @@
 import { Construct, Stack, StackProps } from '@aws-cdk/core';
+import { Vpc, SubnetType } from '@aws-cdk/aws-ec2';
 
 export class EksCdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // VPC
+    const vpc = new Vpc(this, 'EksVpc', {
+      cidr: '10.0.0.0/16',
+      natGateways: 1,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'Public',
+          subnetType: SubnetType.PUBLIC,
+        },
+        {
+          cidrMask: 24,
+          name: 'Private',
+          subnetType: SubnetType.PRIVATE,
+        },
+      ],
+    });
   }
 }

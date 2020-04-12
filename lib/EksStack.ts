ba2574fb51e6e158725eb905a9b7d5ca900450a8
HeRoMo <hero@asterisk-works.jp>
2020-04-12 16:12:39 +0900

    add class ClusterAutoscaler

--- lib/EksStack.ts
+++ lib/EksStack.ts
@@ -17,6 +17,7 @@ import { appDomain } from './config';
 import { createPolicy } from './policies/PolicyUtils';
 import { AlbIngressController } from './k8sResources/AlbIngressController';
 import { MetricsServer } from './k8sResources/MetricsServer';
+import { ClusterAutoscaler } from './k8sResources/ClusterAutoscaler';
 
 /**
  * Create EKS cluster with kubernetes resources related with AWS resources
@@ -80,20 +81,7 @@ export class EksStack extends BaseStack {
    * @param nodeGroup
    */
   private appendClusterAutoscaler(nodeGroup: Nodegroup): void {
-    const policy = createPolicy(this, 'ClusterAutoScalerPolicy', 'cluster-autoscaler.json');
-    nodeGroup.role.attachInlinePolicy(policy);
-
-    const fileName = path.join(__dirname, '..', 'kubernetes-manifests', 'cluster-autoscaler', 'cluster-autoscaler-autodiscover.yaml');
-    const manifests = loadManifestYaml(fileName);
-    const deployment = manifests.find((manifest) => manifest.kind === 'Deployment');
-    const { command } = deployment.spec.template.spec.containers[0];
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const index = command.findIndex((cmd: any) => /<YOUR CLUSTER NAME>/.test(cmd));
-    command[index] = command[index].replace('<YOUR CLUSTER NAME>', this.cluster.clusterName);
-    command.push('--balance-similar-node-groups');
-    command.push('--skip-nodes-with-system-pods=false');
-
-    this.cluster.addResource('cluster-autoscaler-autodiscover', ...manifests);
+    new ClusterAutoscaler(this, 'cluster-autoscaler', this.cluster, nodeGroup);
   }
 
   /**
